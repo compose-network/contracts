051b413f7683c2f6b9d7c89736fed066e1184ca2
yuriissv <yurii@ssvlabs.io>
2025-11-12 00:44:31 +0100

    feat: cet factory

---
+++ L2/src/bridge/CetFactory.sol
@@ -0,0 +1,87 @@
+// SPDX-License-Identifier: GPL-3
+pragma solidity 0.8.30;
+
+import { ComposableERC20 } from "@ssv/src/bridge/ComposableErc20.sol";
+import { ICETFactory } from "@ssv/src/bridge/interfaces/ICetFactory.sol";
+
+contract CetFactory is ICETFactory {
+    function computeSalt(address l1Asset, uint256 remoteChainID) public pure returns (bytes32) {
+        return keccak256(abi.encode(l1Asset, remoteChainID));
+    }
+
+    function predictAddress(
+        address l1Asset,
+        uint256 remoteChainID,
+        uint8 decimals,
+        string memory name,
+        string memory symbol,
+        address bridge
+    ) public view returns (address) {
+        bytes32 salt = computeSalt(l1Asset, remoteChainID);
+
+        bytes memory ctorArgs = abi.encode(
+            l1Asset,
+            remoteChainID,
+            name,
+            symbol,
+            decimals,
+            bridge
+        );
+
+        bytes32 creationHash = keccak256(
+            abi.encodePacked(type(ComposableERC20).creationCode, ctorArgs)
+        );
+
+        return address(
+            uint160(
+                uint256(
+                    keccak256(
+                        abi.encodePacked(
+                            bytes1(0xff),
+                            address(this),
+                            salt,
+                            creationHash
+                        )
+                    )
+                )
+            )
+        );
+    }
+
+    function deployIfAbsent(
+        address l1Asset,
+        uint256 remoteChainID,
+        uint8 decimals,
+        string calldata name,
+        string calldata symbol,
+        address bridge
+    ) external returns (address deployed) {
+        bytes32 salt = computeSalt(l1Asset, remoteChainID);
+
+        address predicted = predictAddress(
+            l1Asset,
+            remoteChainID,
+            decimals,
+            name,
+            symbol,
+            bridge
+        );
+
+        if (predicted.code.length > 0) {
+            return predicted;
+        }
+
+        deployed = address(
+            new ComposableERC20{salt: salt}(
+                l1Asset,
+                remoteChainID,
+                name,
+                symbol,
+                decimals,
+                bridge
+            )
+        );
+
+        return deployed;
+    }
+}
---
+++ L2/src/bridge/interfaces/ICETFactory.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: GPL-3
+pragma solidity 0.8.30;
+
+interface ICETFactory {
+    error DeploymentFailed();
+    error DeployedCetAddressMismatch();
+
+    function computeSalt(address l1Asset, uint256 remoteChainID) external pure returns (bytes32);
+    function predictAddress(
+        address l1Asset,
+        uint256 remoteChainID,
+        uint8 decimals,
+        string memory name,
+        string memory symbol,
+        address bridge
+    ) external view returns (address);
+    function deployIfAbsent(
+        address l1Asset,
+        uint256 remoteChainID,
+        uint8 decimals,
+        string calldata name,
+        string calldata symbol,
+        address bridge
+    ) external returns (address deployed);
+}
